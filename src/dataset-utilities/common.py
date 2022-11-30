ac7813e5021ea0f46660106ba08dd6cfcc180cdc
vlachvojta <vojtik.vlach@seznam.cz>
2022-12-01 00:41:06 +0100

    dataset_util Read from file, append to input database...

--- src/dataset-utilities/common.py
+++ src/dataset-utilities/common.py
@@ -4,19 +4,39 @@
 import pathlib
 import os
 import sys
+import json
 
 
 class Common:
     """Class for common tasks with dataset."""
 
     @staticmethod
-    def print_dict(data: dict):
+    def print_dict(data: dict, files: bool = False):
+        """Print dictionary in a way I like.
+
+        if files, print `data[files]` the first
+        if sort, sort every list in `data.values()`
+        """
         print('{')
-        keys = sorted(list(data.keys()))
-        print(keys)
+
+        if files and 'files' in list(data.keys()):
+            keys = sorted(list(data.keys()))
+            keys.pop(keys.index('files'))
+            print(keys)
+            print(f'\tfiles: {data["files"]}')
+        else:
+            keys = sorted(list(data.keys()))
+            print(keys)
+
         for key in keys:
-            print(f'\t{key}: {data[key]}')
+            print(f'\t{key}:')
+            print(f'\t\t{data[key]}')
         print('}')
+
+    @staticmethod
+    def check_existing_files(files):
+        """Return only existing file"""
+        return [file for file in files if os.path.exists(file)]
 
     @staticmethod
     def get_existing_file_names(files, dirs, exts, recursive):
@@ -36,3 +56,31 @@ class Common:
             # TODO Continue implementing here.
 
         return existing_files
+
+    @staticmethod
+    def save_dict_as_json(data, file):
+        with open(file, 'w') as fp:
+            json.dump(data, fp)
+
+    @staticmethod
+    def read_file(file: str):
+        """Read file, method assumes, file exists."""
+        if not os.path.exists(file):
+            return
+
+        file_extension = file.split('.')[-1]
+        with open(file) as f:
+            if file_extension == 'json':
+                print(f'file_extension json found')
+                data = json.load(f)
+            else:
+                data = f.read()
+        return data
+
+    @staticmethod
+    def get_lines(file):
+        data = Common.read_file(file)
+        if data:
+            return data.split('\n')
+        else:
+            return []
--- src/dataset-utilities/get_unique_symbols.py
+++ src/dataset-utilities/get_unique_symbols.py
@@ -5,6 +5,7 @@ import re
 import sys
 import os
 from common import Common
+import time
 
 
 class Get_unique_symbols:
@@ -16,21 +17,52 @@ class Get_unique_symbols:
     unique_symbols = {}
 
     def __init__(self, files: list = [], dirs: str = '',
-                 exts: str = '', recursive: bool = False):
-        if recursive:
-            print("Recursive lookup has NOT been implemented yet!!!",
-                  file=sys.stderr)
+                 exts: str = '', recursive: bool = False,
+                 database: str = 'all_unique_symbols.json',
+                 input_file: str = 'files.txt'):
+        # if recursive:
+        #     print("Recursive lookup has NOT been implemented yet!!!",
+        #           file=sys.stderr)
+        # self.file_names = Common.get_existing_file_names(files, dirs,
+        #                                                  exts, recursive)
+        print('Hello from GET_UNIQUE_SYMBOLS')
+        self.load_start_data(database)
+        loaded_symbols_sum = self.count_unique_symbols()
 
-        self.file_names = Common.get_existing_file_names(files, dirs,
-                                                         exts, recursive)
-        print(self.file_names)
+        files += Common.get_lines(input_file)
+        self.file_names = Common.check_existing_files(files)
+
+        # self.unique_symbols['files'] = self.file_names
+        print(f'Getting data from {len(self.file_names)} files '
+              f'(every dot is a parsed file).')
+
         for file in self.file_names:
             self.get_symbols_from_file(file)
+        print('')
+
+        total_symbols = self.count_unique_symbols()
+        new_symbols_count = total_symbols - loaded_symbols_sum
+        print(f'{new_symbols_count} new symbols discovered. '
+              f'({total_symbols} total)')
+
+    def load_start_data(self, file):
+        db = Common.read_file(file)
+        # print(type(db))
+        # print(type(db[list(db.keys())[0]]))
+        # print(db[list(db.keys())[0]])
+
+        # Check if data has correct format
+        if isinstance(db, dict):
+            types = [k for k in list(db.keys()) if not isinstance(db[k], list)]
+            if types == []:
+                for k, v in db.items():
+                    self.unique_symbols[k] = set(v)
 
     def get_symbols_from_file(self, file: str):
-        print('getting things from file', file)
+        """Get symbols, save or append to `self.unique_symbols`."""
+        print('.', end='')
 
-        file_data = self.read_file(file)
+        file_data = Common.read_file(file)
         if not file_data:
             return
 
@@ -49,58 +81,64 @@ class Get_unique_symbols:
             else:
                 unique_symbols[col] = {sym}
 
-        print('unique_symbols:')
-        print(unique_symbols)
-
         for col in list(unique_symbols.keys()):
             if col in self.unique_symbols.keys():
-                self.unique_symbols[col].update(unique_symbols[col])
+                try:
+                    self.unique_symbols[col].update(unique_symbols[col])
+                except AttributeError:
+                    self.unique_symbols[col] = set(self.unique_symbols[col])
+                    self.unique_symbols[col].update(unique_symbols[col])
             else:
                 self.unique_symbols[col] = unique_symbols[col]
-        print('self.unique_symbols:')
-        print(self.unique_symbols)
 
-        return
-
-    def read_file(self, file: str):
-        """Read file, method assumes, file exists."""
-        with open(file) as f:
-            data = f.read()
-        return data
+    def count_unique_symbols(self):
+        """Return sum of all unique symbols found."""
+        sum = 0
+        for key in list(self.unique_symbols.keys()):
+            sum += len(self.unique_symbols[key])
+        return sum
 
     def finalize(self, file='stdout'):
+        for key in list(self.unique_symbols.keys()):
+            self.unique_symbols[key] = list(self.unique_symbols[key])
+
         if file == 'stdout':
             print('\n' + '========================' * 3 + '\n')
             self.print_symbols()
         else:
-            print('This type of output has NOT been implemented yet.',
-                  file=sys.stderr)
+            Common.save_dict_as_json(self.unique_symbols, file)
 
     def print_symbols(self):
-        Common.print_dict(self.unique_symbols)
+        Common.print_dict(self.unique_symbols, files=True)
 
 
 def parseargs():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-f", "--files", nargs='*',
+        "-f", "--files", nargs='*', default=[],
         help=("Files to read symbols from, you can add more files.\n" +
               "USE FULL FILE PATH (relative or absolute)"))
-    parser.add_argument(
-        "-e", "--extensions", nargs='*', default=['semantic', 'agnostic'],
-        help=("Set file extensions for files in given folder\n" +
-              "Use in combination with --directories."))
-    parser.add_argument(
-        "-d", "--directories", nargs='*', default=[],
-        help=("Directories where to look for files with given extensions. \n" +
-              "Use in combination with --extensions."))
+    # parser.add_argument(
+    #     "-e", "--extensions", nargs='*', default=['semantic', 'agnostic'],
+    #     help=("Set file extensions for files in given folder\n" +
+    #           "Use in combination with --directories."))
+    # parser.add_argument(
+    #     "-d", "--directories", nargs='*', default=[],
+    #     help=("Directories where to look for files with given extensions. \n" +
+    #           "Use in combination with --extensions."))
     # parser.add_argument(
     #     "-r", "--recursive", default=False, action='store_true',
     #     help=("Activate recursive search in given directory.\n" +
     #           "If not set, use only files in given directory"))
     parser.add_argument(
+        "-d", "--database", default="all_unique_symbols.json",
+        help="Database with already found unique_symbols.")
+    parser.add_argument(
         "-o", "--output", default='stdout',
         help="Set output file with extension. Output format is JSON")
+    parser.add_argument(
+        "-i", "--input_file", default="files.txt",
+        help="File with list of all files to search through.")
 
     # parser.add_argument(
     #     "-o", "--out", default='',
@@ -112,13 +150,19 @@ def main():
     """Main function for simple testing"""
     args = parseargs()
 
+    start = time.time()
     gus = Get_unique_symbols(
         files=args.files,
-        dirs=args.directories,
-        exts=args.extensions)
+        database=args.database,
+        input_file=args.input_file)
+    # dirs=args.directories,
+    # exts=args.extensions)
+
     gus.finalize(args.output)
     # gus.print_symbols(args.out)
 
+    end = time.time()
+    print(f'Total time: {end - start}')
 
 if __name__ == "__main__":
     main()
