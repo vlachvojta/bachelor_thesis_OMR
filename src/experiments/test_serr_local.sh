302a9cf081781d7d9b871a2c0bb31ea8d5fffd07
vlachvojta <vojtik.vlach@seznam.cz>
2023-01-24 12:51:31 +0100

    WIP test_serr_local testing

--- src/experiments/test_serr_local.sh
+++ src/experiments/test_serr_local.sh
@@ -14,7 +14,7 @@ EXPERIMENT="230123_local_decode_pth"
 
 # module add python36-modules-gcc
 pip3 install --upgrade pip
-pip3 install safe_gpu lmdb 
+# pip3 install safe_gpu lmdb opencv-python scipy brnolm
 
 HOME="/home/vlachvojta/skola/BP"
 PERO_PATH=$HOME"/code_from_others/pero"
@@ -23,8 +23,8 @@ export PYTHONPATH=$PERO_PATH:$PERO_OCR_PATH
 export PATH=$PATH:$PYTHONPATH
 
 # Dataset
-LMDB=$HOME"/datasets/images.lmdb"
-DATA_TST=$HOME"/datasets/data_2.tst"
+LMDB=$HOME"/datasets/primus_full_converted_2_lmdb/images.lmdb"
+DATA_TST=$HOME"/datasets/primus_full_converted_2_lmdb/data_2.tst"
 
 # Python scripts
 EXPORT_PY=$PERO_PATH"/pytorch_ctc/export_model.py"
@@ -40,21 +40,23 @@ OCR_MODEL=$CHECKPOINT_PATH"/out.pt"
 PICKLE=$CHECKPOINT"/pickle_out.pkl"
 
 # Run scripts
-python3 $EXPORT_PY  \
-    --path $CHECKPOINT_PATH$CHECKPOINT --net $NET  \
-    --line-height 100 --line-vertical-scale 2705  \
-    --output-json-path $OCR_JSON \
-    --output-model-path $OCR_MODEL  \
-    --trace --device cpu
+# echo "====runnning export_model.py===="
+# python3 $EXPORT_PY  \
+#     --path $CHECKPOINT_PATH$CHECKPOINT --net $NET  \
+#     --line-height 100 --line-vertical-scale 2705  \
+#     --output-json-path $OCR_JSON \
+#     --output-model-path $OCR_MODEL  \
+#     --trace --device cpu
 
+echo "====runnning get_folder_logits.py===="
 python3 $GET_LOGITS_PY  \
     --ocr-json $OCR_JSON  --input $LMDB  \
     --lines $DATA_TST  --output $PICKLE  \
 
+echo "====runnning decode_logtis.py===="
 python3 $DECODE_PY \
     --ocr-json $OCR_JSON \
-    --input $PICKLE
+    --input $PICKLE \
     --report-eta --best --confidence \
-    --use-gpu \
-    --greedy \
+    --greedy
 # What do you mean WHERE (with best and confidence args)
