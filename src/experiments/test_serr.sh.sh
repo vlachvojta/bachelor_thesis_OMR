4fa19d48e0c2326c9c17d0d0009d63656da36f80
vlachvojta <vojtik.vlach@seznam.cz>
2023-02-13 11:35:08 +0100

    update test_serr script with better logging-
    TESTED on Metacentrum

--- src/experiments/test_serr.sh.sh
+++ src/experiments/test_serr.sh.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+#PBS -l select=1:ncpus=2:ngpus=1:mem=15gb:scratch_local=10gb:cluster=black
+#PBS -q gpu@cerit-pbs.cerit-sc.cz
+#PBS -N TODO_EXPERIMENT_NAME_1/2
+#PBS -l walltime=6:0:0
+#PBS -m abe
 
-EXPERIMENT="230125_sagnostic_counting_serr"
+EXPERIMENT="TODO_EXPERIMENT_NAME_1/2"
 cd $SCRATCH
 echo "SCRATCH: "$SCRATCH
 cp -r /storage/brno2/home/xvlach22/bp_omr/datasets $SCRATCH/datasets
@@ -41,6 +46,9 @@ OCR_MODEL=$TMP"/out.pt"
 PICKLE=$TMP"/pickle_out.pkl"
 CONFIDENCE=$TMP"/confidence.del"
 
+echo "ls $CHECKPOINT_PATH/checkpoint_*.pth" | tee -a log_x.txt
+ls $CHECKPOINT_PATH/checkpoint_*.pth | tee -a log_x.txt
+
 pip3.6 install safe_gpu lmdb opencv-python scipy brnolm 1>/dev/null
 pip3.6 install torchvision==0.2.2 1>/dev/null
 
@@ -53,22 +61,22 @@ for checkpoint in `ls $CHECKPOINT_PATH/checkpoint_*.pth`; do
     else
         echo "----runnning export_model.py----"
         python3.6 -u $EXPORT_PY  \
-            --path $CHECKPOINT_PATH$CHECKPOINT --net $NET  \
+            --path $checkpoint --net $NET  \
             --line-height 100 --line-vertical-scale 2705  \
             --output-json-path $OCR_JSON \
             --output-model-path $OCR_MODEL  \
             --trace
-    
+
         echo "----runnning get_folder_logits.py----"
         python3.6 -u $GET_LOGITS_PY  \
             --ocr-json $OCR_JSON --input $LMDB  \
             --lines $DATA_TST --output $PICKLE
 
-        echo "----runnning decode_logtis.py----"
+        echo "----runnning decode_logits.py----"
         python3.6 -u $DECODE_PY \
             --ocr-json $OCR_JSON \
             --input $PICKLE \
-            --report-eta --best $checkpoint.out --greedy \
+            --report-eta --best $checkpoint".out" --greedy \
             --confidence $CONFIDENCE
 
         cp -r $SCRATCH/experiments/$EXPERIMENT /storage/brno2/home/xvlach22/bp_omr/experiments/scratch_copy
@@ -76,10 +84,11 @@ for checkpoint in `ls $CHECKPOINT_PATH/checkpoint_*.pth`; do
         echo "data saved to scratch_copy"
         echo "This checkpoint took: $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
     fi
-done
+done 2>&1 | tee -a log_x.txt
 
 cp -r $SCRATCH/experiments/$EXPERIMENT /storage/brno2/home/xvlach22/bp_omr/experiments/scratch_copy
 clean_scratch
 exit
 
+# TODO shorten log_x.txt
 # TODO evaluate $DECODED with $DATA_TST
