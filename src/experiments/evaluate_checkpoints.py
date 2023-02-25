abb05a528d06b350fea831a1721d91aa63ebdec4
vlachvojta <vojtik.vlach@seznam.cz>
2023-02-25 10:47:17 +0100

    refactor into smaller functions + add json output

--- src/experiments/evaluate_checkpoints.py
+++ src/experiments/evaluate_checkpoints.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.8
 """Simple script to evaluate checkpoint outputs created while training newer version of PERO
 Example run:
-$ python3.8 evaluate_checkpoints.py --ground-truth data.tst \
+$ python3 evaluate_checkpoints.py --ground-truth data.tst \
         --checkpoint-folder checkpoints
 """
 
@@ -12,9 +12,8 @@ import os
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-import jiwer
+# import jiwer
 from customwer import CustomWer
-
 
 rel_dir = os.path.dirname(os.path.relpath(__file__))
 sys.path.append(os.path.join(rel_dir, '..', 'dataset-utilities'))
@@ -25,10 +24,12 @@ class EvaulateCheckpoints:
     """Evaluate checkpoints is simple class to evaluate folder
     of training outputs and export to json and chart.
     """
+
+    ERROR_ERROR = 100.42
+
     def __init__(self, input_files: list, ground_truth: str,
                  output_folder: str = 'eval_out',
                  name: str = 'Evaluated_checkpoints') -> None:
-
         print(f'input_files: {input_files}')
 
         # Read Ground_truth
@@ -39,41 +40,52 @@ class EvaulateCheckpoints:
         # Check input files
         input_files = [file for file in input_files if os.path.isfile(file)]
 
-        wers = []
-        iterations = []
-
+        results = {}
         for file_name in input_files:
-            file = Common.read_file(file_name)
-            file = [line for line in re.split(r'\n', file) if not line == '']
-
-            if len(ground_truth) != len(file):
-                continue
-
-            wer_list = []
-            for gt, pred in zip(ground_truth, file):
-                wer_list.append(jiwer.wer(gt, pred) * 100)
-
-            # Custom wer with continues counting
-            wer_list = []
-            my_wer = CustomWer()
-            
-            # Add lines in to different ways to demonstrate
-            for gt, pred in zip(ground_truth[:50], file[:50]):
-                my_wer.add_lines(gt, pred)
-            my_wer.add_lines(ground_truth[50:], file[50:])
-
-            wer_list.append(my_wer())
-
-            cerr = self.get_cerr_mean(ground_truth, file)
-
             iteration = int(re.findall(r"\d+", file_name)[-1])
-            iterations.append(iteration)
-            print(f'Iteration: {iteration} wer: {my_wer():.2f}%')
-            wers.append(my_wer())
 
-        print(f'Iterations: \t{iterations}')
-        print(f'WERs: \t\t{wers}')
+            wer, cer = self.get_errs(file_name, ground_truth)
 
+            results[iteration] = {'iter': iteration, 'wer': wer, 'cer': cer}
+            print(f'Iteration: {iteration} wer: {results[iteration]}')
+
+        self.make_chart(results, output_folder, name)
+
+        json_file_path = os.path.join(output_folder, name + '.json')
+        Common.write_to_file(results, json_file_path)
+
+        print(f'Chart and json file saved to {output_folder}')
+
+    def get_errs(self, file_name, ground_truth):
+        """Get WER and CER of file."""
+        file = Common.read_file(file_name)
+        file = [line for line in re.split(r'\n', file) if not line == '']
+
+        if len(ground_truth) != len(file):
+            return self.ERROR_ERROR, self.ERROR_ERROR
+
+        # Custom wer with continues counting
+        my_wer = CustomWer()
+
+        # Add lines in two different ways to demonstrate
+        for gt, pred in zip(ground_truth[:50], file[:50]):
+            my_wer.add_lines(gt, pred)
+        my_wer.add_lines(ground_truth[50:], file[50:])
+
+        cer = self.get_cerr(ground_truth, file)
+        return my_wer(), cer
+
+    def get_cerr(self, truth, result) -> float:
+        # TODO Count Levenshtein distance same as in PERO
+        return self.ERROR_ERROR
+    
+    def make_chart(self, results, output_folder, name):
+        """Generate chart with iterations, WERs and CERs"""
+        iterations = [results[res]['iter'] for res in results]
+        wers = [results[res]['wer'] for res in results]
+        cers = [results[res]['cer'] for res in results]
+        
+        # TODO add CER to chart
         plt.title(name)
         plt.plot(np.array(iterations), np.array(wers))
         plt.xlabel('Iteration')
@@ -83,13 +95,6 @@ class EvaulateCheckpoints:
             os.makedirs(output_folder)
 
         plt.savefig(os.path.join(output_folder, name + '.png'))
-        # TODO save also JSON log to output_folder
-
-        print(f'Stuff saved to {output_folder}')
-
-    def get_cerr_mean(self, truth, result) -> float:
-        # TODO Count Levenshtein distance
-        return 0
 
 
 def parseargs():
@@ -102,7 +107,7 @@ def parseargs():
         "-g", "--ground-truth", required=True,
         help="Ground truth to compare files with.")
     parser.add_argument(
-        "-o", "--output-folder", type=str, default='eval_out',
+        "-o", "--output-folder", type=str, default='evaluated_checkpoints',
         help="Output folder to write outputs to.")
     parser.add_argument(
         "-n", "--name", type=str, default='Evaluated_checkpoints',
