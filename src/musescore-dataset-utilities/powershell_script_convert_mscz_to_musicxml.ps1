9f0daef0c819850fb2579ee5894fac8966688482
vlachvojta <vojtik.vlach@seznam.cz>
2023-03-15 15:12:40 +0100

    add param options for more comfortable usage

--- src/musescore-dataset-utilities/powershell_script_convert_mscz_to_musicxml.ps1
+++ src/musescore-dataset-utilities/powershell_script_convert_mscz_to_musicxml.ps1
@@ -1,16 +1,28 @@
+param ($in_ext, $out_ext, $in_dir='.') #, $out_dir='out')
+
+echo "Welcome to simple powershel script with parameters"
+if ($in_ext -eq $null){
+	$in_ext = read-host -Prompt "Please enter the INPUT files extension"
+} else {
+	echo "input files extension: $in_ext"
+}
+if ($out_ext -eq $null){
+	$out_ext = read-host -Prompt "Please enter the OUTPUT files extension"
+} else {
+	echo "output files extension: $out_ext"
+}
+
+echo "directory with input files: $in_dir"
+# echo "output directory: $out_dir"
+
 # $DIR = "D:\OneDrive - Vysoké učení technické v Brně\skola\BP\datasets\musescore_pipeline_44\1_musicxml_using_MuseScore4_native_CLI"
-$DIR = "."
 
-echo "Hellou"
-
-echo $DIR
-
-Get-ChildItem $DIR -Filter *.mscz | 
+Get-ChildItem $in_dir -Filter *.$in_ext | 
 ForEach-Object { 
-	if (-not(Test-Path "$($DIR)$($_).musicxml")) 
+	if (-not(Test-Path "$($DIR)$($_).$out_ext")) 
 		{ 
 			echo $_ ; 
-			musescore4 --export-to "$($_.FullName).musicxml" $_.FullName 
+			musescore4 $_.FullName --export-to "$($_.FullName).$out_ext"
 		} 
 	else
 		{
