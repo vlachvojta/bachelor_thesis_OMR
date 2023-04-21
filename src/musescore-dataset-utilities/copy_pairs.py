a5102f3113fa612369949e0a8196b17b74fd74f0
vlachvojta <vojtik.vlach@seznam.cz>
2023-04-21 03:05:49 +0200

    add option for specifying input folder and more input list files

--- src/musescore-dataset-utilities/copy_pairs.py
+++ src/musescore-dataset-utilities/copy_pairs.py
@@ -2,8 +2,8 @@
 """Load label and image pairs given by input file.
 
 Usage:
-$ python3 copy_pairs.py -i 6_copied_pairs/big_density_systems.txt -o 6_copied_pairs_subset
-Copies pairs of labels and images with name in input file to 6_copied_pairs_subset.
+$ python3 copy_pairs.py -i 6_copied_pairs/ -I big_density_systems.txt -o 6_copied_pairs_subset
+Copies pairs of labels and images with name in big_density_systems.txt to 6_copied_pairs_subset.
 """
 
 import argparse
@@ -25,14 +25,16 @@ class PairCopier:
 
     EMPTY_SYSTEM_ID = 'EMPTY_SYSTEM_ID'
 
-    def __init__(self, input_file: str, output_folder: str, verbose: bool = False):
-        self.input_file = input_file
+    def __init__(self, input_files: str, output_folder: str, 
+                 input_folder: str = None, verbose: bool = False):
+        self.input_files = input_files
         self.output_folder = output_folder
+        self.input_folder = input_folder if input_folder else os.path.dirname(self.input_files[0])
         self.verbose = verbose
 
-        dirname = os.path.dirname(input_file)
-        self.image_folder = dirname if dirname else '.'
-        self.label_file = os.path.join(dirname, '0_labels.semantic')
+        # dirname = os.path.dirname(input_file)
+        # self.image_folder = dirname if dirname else '.'
+        self.label_file = os.path.join(self.input_folder, '0_labels.semantic')
         self.out_label_file = os.path.join(self.output_folder, '0_labels.semantic')
 
         if verbose:
@@ -41,14 +43,17 @@ class PairCopier:
             logging.basicConfig(level=logging.INFO,format='[%(levelname)-s]\t- %(message)s')
 
         # Load list of images
-        self.images = [img for img in os.listdir(self.image_folder) if img.endswith('.png')]
+        self.images = [img for img in os.listdir(self.input_folder) if img.endswith('.png')]
         print(f'Found {len(self.images)} images.')
 
         self.labels = MusescoreAnalyzer.load_labels(self.label_file)
         print(f'Found {len(self.labels)} labels.')
 
-        self.system_ids = self.load_system_ids(self.input_file)
-        print(f'Found {len(self.system_ids)} system IDs.')
+        self.system_ids = []
+        for input_file in self.input_files:
+            self.system_ids += self.load_system_ids(input_file)
+        self.system_ids = sorted(set(self.system_ids))
+        print(f'Found {len(self.system_ids)} unique system IDs.')
 
         # Create output part if necessary
         if not os.path.exists(self.output_folder):
@@ -63,12 +68,18 @@ class PairCopier:
         success_files = 0
         fail_files = 0
 
+        print(f'Copying {len(self.system_ids)} images and labels from {self.input_folder} '
+              f'to {self.output_folder}')
+        print('\tEvery dot is 200 parts, line is 10_000.')
+
         with open(self.out_label_file, 'w', encoding='utf-8') as out_label_file:
-            for system_id in self.system_ids:
+            for i, system_id in enumerate(self.system_ids):
+                Common.print_dots(i, 200, 10_000)
+
                 if system_id in self.labels and system_id in self.labels:
                     self.copied_labels[system_id] = self.labels[system_id]
 
-                    img_source = os.path.join(self.image_folder, system_id)
+                    img_source = os.path.join(self.input_folder, system_id)
                     img_dest = os.path.join(self.output_folder, system_id)
                     logging.debug(f'Copying image ({img_source}) => ({img_dest})')
                     copyfile(img_source, img_dest)
@@ -113,8 +124,11 @@ def parseargs():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", "--input_file", type=str, required=True,
+        "-I", "--input_files", nargs='+',
         help="File with system_ids of pairs to copy.")
+    parser.add_argument(
+        "-i", "--input-folder", type=str, default=None,
+        help="Input folder to look for pairs in.")
     parser.add_argument(
         "-o", "--output-folder", type=str, required=True,
         help="Output folder to copy selected pairs to.")
@@ -130,7 +144,8 @@ def main():
     start = time.time()
 
     copier = PairCopier(
-        input_file=args.input_file,
+        input_files=args.input_files,
+        input_folder=args.input_folder,
         output_folder=args.output_folder,
         verbose=args.verbose)
     copier()
