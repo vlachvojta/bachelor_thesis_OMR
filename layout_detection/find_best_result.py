a665b6a34bb3ca96a1381337007b351ee6877f01
vlachvojta <vojtik.vlach@seznam.cz>
2023-09-07 10:19:33 +0200

    Add better printing stuff.

--- layout_detection/find_best_result.py
+++ layout_detection/find_best_result.py
@@ -8,6 +8,7 @@ import sys
 import os
 import time
 import csv
+import re
 
 
 def parseargs():
@@ -38,6 +39,7 @@ def main():
 
 class BestResultFinder:
     best_results = {}
+    best_results_with_checkpoint = {}
 
     """Get label-image pairs from two separate folders."""
     def __init__(self, path: str = ''):
@@ -58,14 +60,21 @@ class BestResultFinder:
         for folder in folders:
             self.find_best_result(path, folder, 'results.csv')
 
-        print(self.best_results)
+        all_folders = set(list(self.best_results.keys()) + list(self.best_results_with_checkpoint.keys()))
+        print(f'All folders: {all_folders}')
 
-        best_result = max(self.best_results, key=lambda x: self.best_results[x]['result'])
         print()
-        print(f'Best result: {best_result} ')
-        print(f'\tin epoch {self.best_results[best_result]["epoch"]}')
-        print(f'\twith metric {self.best_results[best_result]["result"]}')
-        print()
+        print('Best results in folders:')
+        for folder in sorted(all_folders):
+            print(f'{folder}')
+            if folder in self.best_results_with_checkpoint:
+                print(f'\t {self.best_results_with_checkpoint[folder]["result"]} '
+                      f'in epoch {self.best_results[folder]["epoch"]}')
+            if folder in self.best_results:
+                print(f'\t({self.best_results[folder]["result"]} in epoch {self.best_results[folder]["epoch"]})')
+
+        BestResultFinder.print_best_result(self.best_results)
+        BestResultFinder.print_best_result(self.best_results_with_checkpoint, with_checkpoint=True)
 
     def find_best_result(self, path, folder, results_name: str):
         results_file = os.path.join(path, folder, 'results.csv')
@@ -84,6 +93,36 @@ class BestResultFinder:
 
         best_epoch = max(results, key=results.get)
         self.best_results[folder] = {'epoch': best_epoch, 'result': results[best_epoch]}
+
+        checkpoints = os.listdir(os.path.join(path, folder, 'weights'))
+        pattern = r'[a-zA-Z]*(\d+)\.pt\S*'
+        checkpoints = [re.match(pattern, c).group(1) for c in checkpoints if re.match(pattern, c)]
+        checkpoints = [int(checkpoint) for checkpoint in checkpoints]
+        # print(checkpoints)
+
+        results_with_checkpoints = {}
+        for checkpoint in checkpoints:
+            results_with_checkpoints[checkpoint] = results[checkpoint]
+
+        # print()
+        # print(results_with_checkpoints)
+
+        best_epoch = max(results_with_checkpoints, key=results_with_checkpoints.get)
+        self.best_results_with_checkpoint[folder] = {'epoch': best_epoch, 'result': results[best_epoch]}
+
+    @staticmethod
+    def print_best_result(result_dict, with_checkpoint: bool = False):
+        best_result = max(result_dict, key=lambda x: result_dict[x]['result'])
+
+        print()
+        if with_checkpoint:
+            print(f'Best result WITH checkpoint:')  # {result_dict} ')
+        else:
+            print(f'Best result:')  # {result_dict} ')
+        epoch = result_dict[best_result]["epoch"]
+        print(f'\t{result_dict[best_result]["result"]} in {best_result}/{epoch}')
+        print(f'\t=>{best_result}/weights/epoch{epoch}.pt')
+        print()
 
 
 if __name__ == "__main__":
