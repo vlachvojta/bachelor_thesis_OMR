c65661a98191f5798af83a315f475305f674b76f
vlachvojta <vojtik.vlach@seznam.cz>
2024-08-15 17:10:25 +0200

    Fix location for calling pipeline_100 scripts

--- musescore-dataset-utilities/shell_pipelines/run_pipeline_100.sh
+++ musescore-dataset-utilities/shell_pipelines/run_pipeline_100.sh
@@ -17,15 +17,12 @@ sub_dir=$(get_arg $2 '')
 
 time_start=$(date +%s)
 
-# ./run_0_to_1_musescore3.sh 0_orig_mscz/$sub_folder 1_musicxml_by_M3_CLI/$sub_folder  && \
-# ./run_1_to_2.sh 1_musicxml_by_M3_CLI/$sub_folder 2_musicxml_parts/$sub_folder  && \
-$LOCAL/run_0_to_1_musescore4.sh $data_dir/0_orig_mscz/$sub_dir $data_dir/1_musicxml_by_M4_CLI/$sub_dir  && \
-$LOCAL/run_1_to_2.sh $data_dir/1_musicxml_by_M4_CLI/$sub_dir $data_dir/2_musicxml_parts/$sub_dir  && \
-$LOCAL/run_2_to_3_musescore4.sh $data_dir/2_musicxml_parts/$sub_dir $data_dir/3_img_pages/$sub_dir  && \
-# ./run_2_to_3_musescore3.sh 2_musicxml_parts/$sub_folder 3_img_pages/$sub_folder  && \ 
-$LOCAL/run_3_to_4.sh $data_dir/3_img_pages/$sub_dir $data_dir/4_img_staves/$sub_dir  && \
-$LOCAL/run_2_to_5.sh $data_dir/2_musicxml_parts/$sub_dir $data_dir/5_labels_semantic/$sub_dir  && \
-$LOCAL/run_5_to_6.sh $data_dir/4_img_staves/$sub_dir $data_dir/5_labels_semantic/$sub_dir $data_dir/6_copied_pairs/$sub_dir  && \
+$LOCAL/100/run_0_to_1_musescore4.sh $data_dir/0_orig_mscz/$sub_dir $data_dir/1_musicxml_by_M4_CLI/$sub_dir  && \
+$LOCAL/100/run_1_to_2.sh $data_dir/1_musicxml_by_M4_CLI/$sub_dir $data_dir/2_musicxml_parts/$sub_dir  && \
+$LOCAL/100/run_2_to_3_musescore4.sh $data_dir/2_musicxml_parts/$sub_dir $data_dir/3_img_pages/$sub_dir  && \
+$LOCAL/100/run_3_to_4.sh $data_dir/3_img_pages/$sub_dir $data_dir/4_img_staves/$sub_dir  && \
+$LOCAL/100/run_2_to_5.sh $data_dir/2_musicxml_parts/$sub_dir $data_dir/5_labels_semantic/$sub_dir  && \
+$LOCAL/100/run_5_to_6.sh $data_dir/4_img_staves/$sub_dir $data_dir/5_labels_semantic/$sub_dir $data_dir/6_copied_pairs/$sub_dir  && \
 # ./run_6_to_7.sh
 
 echo ""
