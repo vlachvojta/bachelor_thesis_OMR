5939bd22d1aed4fcbb6de71b8aff0f281ab6c5dc
vlachvojta <vojtik.vlach@seznam.cz>
2024-08-19 15:23:34 +0200

    Move generating labels back for backwards compatibility

--- musescore-dataset-utilities/shell_pipelines/run_pipeline_100.sh
+++ musescore-dataset-utilities/shell_pipelines/run_pipeline_100.sh
@@ -19,12 +19,15 @@ time_start=$(date +%s)
 
 $LOCAL/100/run_0_to_1_musescore4.sh $data_dir/0_orig_mscz/$sub_dir $data_dir/1_musicxml_by_M4_CLI/$sub_dir  && \
 $LOCAL/100/run_1_to_2.sh $data_dir/1_musicxml_by_M4_CLI/$sub_dir $data_dir/2_musicxml_parts/$sub_dir  && \
-$LOCAL/100/run_2_to_5.sh $data_dir/2_musicxml_parts/$sub_dir $data_dir/5_labels_semantic/$sub_dir  && \
 $LOCAL/100/run_2_to_3_musescore4.sh $data_dir/2_musicxml_parts/$sub_dir $data_dir/3_img_pages/$sub_dir  && \
 $LOCAL/100/run_3_to_4.sh $data_dir/3_img_pages/$sub_dir $data_dir/4_img_staves/$sub_dir  && \
+$LOCAL/100/run_2_to_5.sh $data_dir/2_musicxml_parts/$sub_dir/done $data_dir/5_labels_semantic/$sub_dir  && \
 $LOCAL/100/run_5_to_6.sh $data_dir/4_img_staves/$sub_dir $data_dir/5_labels_semantic/$sub_dir $data_dir/6_copied_pairs/$sub_dir
 
 # $LOCAL/100/run_6_to_7.sh $data_dir/6_copied_pairs/$sub_dir
+
+exit_code=$?
+echo "pipeline scripts exited with: $exit_code"
 
 echo ""
 echo "Pipeline 100 complete"
