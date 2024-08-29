4f93246212f070bbed5ed91e85318bb10efcf7ea
vlachvojta <vojtik.vlach@seznam.cz>
2024-08-29 11:41:29 +0200

    Add choosable suffix to the dir zipping snippet

--- musescore-dataset-utilities/shell_pipelines/usefull_bash_snippets.sh
+++ musescore-dataset-utilities/shell_pipelines/usefull_bash_snippets.sh
@@ -7,9 +7,11 @@ for dir in $(ls -d */ | cut -f1 -d'/'); do \
     cd .. ; \
 done
 
-# for every dir in the current directory, zip all png files to zip file with the same name as the directory
+# for every dir in the current directory, zip files with chosen suffix (or all using suffix="")
+# to zip file with the same name as the directory
+suffix=".png"; \
 for dir in $(ls -d */ | cut -f1 -d'/'); do \
     echo "Zipping $dir"; \
-    zip -r "$dir.zip" $dir/*.png > /dev/null; \
+    zip -r "$dir.zip" $dir/*${suffix} > /dev/null; \
 done
 
