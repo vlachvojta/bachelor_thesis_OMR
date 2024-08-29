892681b1a6bc32f69b8856c56e9f4329d47c1153
vlachvojta <vojtik.vlach@seznam.cz>
2024-08-29 10:57:12 +0200

    Add zipping bash snippet

--- musescore-dataset-utilities/shell_pipelines/usefull_bash_snippets.sh
+++ musescore-dataset-utilities/shell_pipelines/usefull_bash_snippets.sh
@@ -6,3 +6,10 @@ for dir in $(ls -d */ | cut -f1 -d'/'); do \
     zip -rg ../images.zip *.png > /dev/null; \
     cd .. ; \
 done
+
+# for every dir in the current directory, zip all png files to zip file with the same name as the directory
+for dir in $(ls -d */ | cut -f1 -d'/'); do \
+    echo "Zipping $dir"; \
+    zip -r "$dir.zip" $dir/*.png > /dev/null; \
+done
+
